4517976d13a61f8902dea6d6173cd9d46492403a
Roberto Carlos da Silva <robertosistemas@hotmail.com>
2022-10-24 11:22:29 -0300

    Teste unitário para o método main

---
+++ test/CalcularTroco.Test/ProgramTest.cs
@@ -0,0 +1,29 @@
+﻿namespace CalcularTroco.Test;
+
+public class ProgramTest
+{
+    [Fact]
+    public void MainTest()
+    {
+        using StringReader reader = new("21,45");
+        Console.SetIn(reader);
+        using StringWriter writer = new();
+        Console.SetOut(writer);
+        Program.Main(Array.Empty<string>());
+        writer.Flush();
+        var result = writer.GetStringBuilder().ToString().Trim();
+        var resultArray = result.Split(Environment.NewLine).ToList();
+
+        var expected = new List<string>
+            {
+                "Entre com o valor maior que zero:",
+                "Calculando o menor número de notas e moedas para o troco de: 21.45",
+                "1 nota de 20 reais",
+                "1 moeda de 1 real",
+                "1 moeda de 25.00 centavos",
+                "2 moedas de 10.00 centavos"
+            };
+        Assert.Equal(expected.Count, resultArray.Count);
+        Assert.Equal(expected, resultArray);
+    }
+}
